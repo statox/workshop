0887ec5762917be72cad01d5ed5572c0931850d1
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-06 22:56:41 +0100

    Remove stuff from template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,3 @@
-<script>
-    import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
-    import Game from '$lib/components/Game.svelte';
-    import Markdown from '$lib/components/Markdown/Main.svelte';
-</script>
+<h2>Home</h2>
 
-<Tabs>
-    <TabList>
-        <Tab>Game</Tab>
-        <Tab>Markdown</Tab>
-    </TabList>
-
-    <TabPanel>
-        <Game />
-    </TabPanel>
-    <TabPanel>
-        <Markdown />
-    </TabPanel>
-</Tabs>
+<p>Here be lardons</p>
