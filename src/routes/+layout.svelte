3bd2143db4b31e448ac9cb9bd67f9c6557ecef76
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-06 22:56:41 +0100

    Remove stuff from template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <header>
-    <h1>P5+Svelte template</h1>
+    <h1>Workshop</h1>
 
     <nav>
         <a href="/">Home</a> |
@@ -7,10 +7,3 @@
 </header>
 
 <slot />
-
-<style>
-    :global(.svelte-tabs li.svelte-tabs__tab) {
-        background-color: #bfbec6;
-        margin-right: 4px;
-    }
-</style>
