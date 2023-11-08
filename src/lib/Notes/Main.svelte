79fd61f9711d573fe15cadc4ffd6428b2bcee5b3
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-08 19:04:38 +0100

    Notes: add search

--- src/lib/Notes/Main.svelte
+++ src/lib/Notes/Main.svelte
@@ -3,6 +3,7 @@
     import type { Note } from './types';
 
     const notesModules = import.meta.glob("./data/*.md");
+    let searchStr: string;
 
     const extractDataFromSource = (source: string) => {
         const titleMatch = source.match(/\[title]: # '(.*)'\n/);
@@ -29,9 +30,31 @@
             notes = [...notes, data];
         });
     }
+
+    const filterIn = (note: Note, searchStr: string) => {
+        const search = searchStr?.toLowerCase().trim() || '';
+        if (!searchStr || searchStr.length === 0) {
+            return true;
+        }
+        if (note.title.toLowerCase().includes(search)) {
+            return true;
+        }
+        for (const tag of note.tags) {
+            if (tag.toLowerCase().includes(search)) {
+                return true;
+            }
+        }
+        return false;
+    }
 </script>
-<h2>Notes</h2>
+
+<h2>Stuff I forget</h2>
+<div>
+    Search by tag: <input type="text" bind:value={searchStr}>
+</div>
 
 {#each notes as note}
+    {#if filterIn(note, searchStr) }
     <NoteComponent {note} />
+    {/if}
 {/each}
