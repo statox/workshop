087333dd837bb525ae5e7f2acd9498697ba5f033
statox <me@statox.fr>
2023-11-08 19:37:00 +0000

    Deploying to gh-pages from @ statox/workshop@9fb94ecf48772b1c466ab828f1146b66f8941c9e 🚀

---
+++ _app/immutable/chunks/ansible.151520b7.js
@@ -0,0 +1,40 @@
+const e=`[tags]: # '["bash", "ansible"]'
+[title]: # 'Ansible tips'
+
+### Run a command on several hosts
+
+The documentation [Introduction to Ad Hoc commands](https://docs.ansible.com/ansible/latest/user_guide/intro_adhoc.html) is a great reference.
+
+1. In the inventory file create the list of servers to target
+
+\`\`\`yml
+[my_target_group_name]
+host01.domain.ext
+host02.domain.ext
+host03.domain.ext
+\`\`\`
+
+2. Run the command
+
+\`\`\`bash
+ansible [target_group_name] -i path/to/inventory/file -m shell -a [your_command]
+
+# Example
+ansible my_target_group_name -i inventories/inventory -m shell -a 'df -h'
+\`\`\`
+
+The module \`shell\` allows to run any shell command but one can use [any module](https://docs.ansible.com/ansible/latest/collections/index_module.html)
+
+If you need to become root to run the command pass \`--become\` to the \`ansible\` command.
+
+| Parameter                   | Description                                                  |
+| --------------------------- | ------------------------------------------------------------ |
+| \`--become\`                  | Become root                                                  |
+| \`-u <username>\`             | Change the username used                                     |
+| \`--ask-become-pass\` or \`-K\` | Prompts you for the password to use for privilege escalation |
+| \`-f 10\`                     | Change how many machines are targeted simultaneously         |
+
+### Copying files between hosts
+
+Refer to [this link](https://www.middlewareinventory.com/blog/how-to-copy-files-between-remote-servers-ansible-fetch-sync/)
+`;export{e as default};
