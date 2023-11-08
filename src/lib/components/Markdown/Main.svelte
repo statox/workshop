4daaf4d410ee62440de3e34b4545a8d9a5037aa2
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-08 20:36:18 +0100

    Markdown: Add syntax highlighting

--- src/lib/components/Markdown/Main.svelte
+++ src/lib/components/Markdown/Main.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { base } from '$app/paths';
     import { onMount } from 'svelte';
+    import hljs from 'highlight.js';
     import SvelteMarkdown from 'svelte-markdown';
     import ImageRenderer from './renderers/Image.svelte';
     import LinkRenderer from './renderers/Link.svelte';
@@ -12,8 +13,18 @@
         // Shitty hack because I didn't figure out how to tweak the pre-renderer
         // to properly include the baseUrl in the image links
         sourceWithFixedLinks = source.replaceAll('{baseUrl}', base);
+
+        hljs.highlightAll();
     });
 </script>
+
+<svelte:head>
+    <!-- TODO Find a way to avoid adding this line several time when we have multiple Markdown components -->
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.3.2/build/styles/night-owl.min.css"
+    />
+</svelte:head>
 
 <SvelteMarkdown
     source={sourceWithFixedLinks}
