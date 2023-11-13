e4d619b8ac94469fe11b2ef36132d5dcad8b5e91
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-13 01:16:55 +0100

    Chords: Add list of all chords

--- src/lib/Music/Chords/Main.svelte
+++ src/lib/Music/Chords/Main.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { alphaSort } from '$lib/helpers';
     import type { Chord } from './types';
     export let chords: Chord[];
 
+    let searchString = '';
+
     const nbLatestChords = 10;
     const latestChords = chords
-        .filter(c => c.creationDate)
-        .sort((a, b) => {
-            return b.creationDate - a.creationDate;
-        })
-        .slice(0, nbLatestChords);
-
-    const noop = () => {};
-
+    .filter(c => c.creationDate)
+    .sort((a, b) => {
+        return b.creationDate - a.creationDate;
+    })
+    .slice(0, nbLatestChords);
 
     const nbRandomChords = 4;
     let randomChordIndexes: number[] = [];
@@ -40,8 +40,19 @@
         randomChordIndexes = randomChordIndexes;
     };
 
-    onMount(() => {getRandomSongs()});
+    type chordsByArtist = {
+        [artist: string]: Chord[];
+    };
+    const chordsByArtist = chords.reduce((byArtist, chord) => {
+        const artist = chord.artist;
+        if (!byArtist[artist]) {
+            byArtist[artist] = [];
+        }
+        byArtist[artist].push(chord);
+        return byArtist;
+    }, {} as chordsByArtist);
 
+    onMount(() => {getRandomSongs()});
 </script>
 
 <h2>Chords</h2>
@@ -66,13 +77,13 @@
     <h3>Random song</h3>
     <button on:click={getRandomSongs}>Get a random song</button>
     {#if randomChordIndexes.length > nbRandomChords}
-    <button on:click={getPreviousRandomSongs}>&nbsp↶&nbsp</button>
+        <button on:click={getPreviousRandomSongs}>&nbsp↶&nbsp</button>
     {/if}
     <br/>
 
     <ul>
         {#each randomChordIndexes.slice(randomChordIndexes.length - nbRandomChords) as chordIndex}
-        {@const chord = chords[chordIndex]}
+            {@const chord = chords[chordIndex]}
             <li>
                 <span>
                     <a href={chord.url} target="_blank" rel="noopener noreferrer">{chord.artist} - {chord.title}</a>
@@ -82,6 +93,40 @@
     </ul>
 </div>
 <br/>
+
+
+<div>
+    <h3>All chords</h3>
+    Search an artist or a title: <input type="text" bind:value={searchString} >
+    <button on:click={() => searchString = ''}>&nbsp✖&nbsp</button>
+</div>
+<br/>
+
+<table>
+    {#each Object.keys(chordsByArtist).sort(alphaSort) as artist}
+        {@const chords = chordsByArtist[artist].sort((a, b) => a.title < b.title ? -1 : 1) }
+        {@const artistTags = artist + ';' + chords.reduce((tags, chord) => tags + chord.title + ';' + chord.tags.join(','), '')}
+
+        {#if searchString.length === 0 || artistTags.toLowerCase().match(searchString.toLowerCase())}
+            <tr>
+                <td>{artist}
+                    <ul class="ul2col">
+                        {#each chords as chord}
+                            {@const chordTags = artist + ';' + chord.title + ';' + chord.tags.join(',')}
+                            {#if searchString.length === 0 || chordTags.toLowerCase().match(searchString.toLowerCase())}
+                                <li>
+                                    <span>
+                                        <a href={chord.url} target="_blank" rel="noopener noreferrer">{chord.title}</a>
+                                    </span>
+                                </li>
+                            {/if}
+                        {/each}
+                    </ul>
+                </td>
+            </tr>
+        {/if}
+    {/each}
+</table>
 
 <style>
 @media screen and (min-width: 600px) {
---
+++ src/lib/helpers.ts
@@ -0,0 +1,5 @@
+export const alphaSort = (a: string, b: string) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+};
