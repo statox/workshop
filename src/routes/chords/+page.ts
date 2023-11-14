4e99782d73a0d49e9473fffc3237004cc40ed5ed
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-15 00:59:16 +0100

    Chords: Add result of checkChords workflow in button

--- src/routes/chords/+page.ts
+++ src/routes/chords/+page.ts
@@ -1,9 +1,10 @@
 import type { PageLoad } from './$types';
-import type { Chord } from '$lib/Music/Chords//types';
+import type { Chord } from '$lib/Chords//types';
 
 const CHORDS_URL = 'https://raw.githubusercontent.com/statox/blog/master/src/_data/chords.json';
 
-export const load: PageLoad = async (): Promise<{ chords: Chord[] }> => {
+export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastCheckChords: any }> => {
+    const lastCheckChords = await getLastCheckChordsWorkflow();
     const chords = await fetch(CHORDS_URL).then((response) => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -12,6 +13,20 @@ export const load: PageLoad = async (): Promise<{ chords: Chord[] }> => {
         return response.json();
     });
     // TODO Add type checking
-    const data = { chords };
-    return { chords };
+    return { chords, lastCheckChords };
+};
+
+const WORKFLOW_URL = 'https://api.github.com/repos/statox/workshop/actions/runs';
+const getLastCheckChordsWorkflow = async () => {
+    const reponse = await fetch(WORKFLOW_URL, {
+        headers: {
+            Accept: 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+    const result = await reponse.json();
+
+    return result.workflow_runs
+        .filter((run: any) => run.name === 'Check chords.json for dead links')
+        .sort((a: any, b: any) => b.id - a.id)[0];
 };
