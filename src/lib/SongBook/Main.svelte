dde8d9b3845a01e4115f428d9897b889e5efaeaf
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-29 20:36:01 +0100

    ChordsChecks: Rework mecanism

--- src/lib/SongBook/Main.svelte
+++ src/lib/SongBook/Main.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import BackToTop from '$lib/components/BackToTop/Main.svelte';
+    import ChordsChecks from './components/ChordsChecks.svelte';
     import ListByArtist from './components/ListByArtist.svelte';
     import ListByTags from './components/ListByTags.svelte';
     import LatestAdditions from './components/LatestAdditions.svelte';
@@ -7,9 +8,25 @@
     import type { Chord } from './types';
 
     export let chords: Chord[];
-    export let lastCheckChords: any;
+    export let lastChordsCheck: any;
 
-    console.log({lastCheckChords});
+    const chordsChecksIcons = {
+        'error': 'fa fa-question-circle-o',
+        'failures':  'fa fa-exclamation-circle',
+        'ok': 'fa fa-check-circle'
+    };
+    const getChordsCheckStatus = () => {
+        if (!lastChordsCheck) {
+            return 'error';
+        }
+        if (lastChordsCheck.nbFails > 0) {
+            return 'failures';
+        }
+        return 'ok'
+    };
+
+    let showChordsChecks = false;
+    const chordsCheckIcon = chordsChecksIcons[getChordsCheckStatus()];
 
     let searchString = '';
 
@@ -23,6 +40,14 @@
 <h2>
     Song book
     <span class="pull-right">
+        <button style:position='relative' on:click={() => showChordsChecks = !showChordsChecks}>
+            Dead links
+            {#if lastChordsCheck?.nbFails > 0}
+                ({ lastChordsCheck.nbFails })
+            {/if}
+            <span class={chordsCheckIcon}></span>
+        </button>
+
         <a href="https://github.com/statox/blog/issues/105#new_comment_field" target="_blank" rel="noopener noreferrer">
             <button>Add a song</button>
         </a>
@@ -33,6 +58,9 @@
 
 <p>This is a list of {chords.length} song chords I have been gathering for the past 10 years and that I like to play.</p>
 
+{#if showChordsChecks}
+<ChordsChecks {lastChordsCheck} />
+{/if}
 <LatestAdditions {chords} />
 <RandomSongs {chords} />
 
@@ -45,3 +73,24 @@
 
 
 <svelte:component this={views[view]}  {chords} {searchString}/>
+
+<style>
+.fa-exclamation-circle {
+    color: #E82236;
+}
+
+.fa-check-circle {
+    color: #89E07D;
+}
+
+.btn-overlay {
+    position: absolute;
+    padding: 0;
+    top: -100%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #ffffff;
+    transition: opacity .5s;
+}
+</style>
