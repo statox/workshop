115f7773f58077a09dd6da62135f7d73b7ecc871
Adrien Fabre <adrien.fabre@dashlane.com>
2023-11-29 20:36:01 +0100

    ChordsChecks: Rework mecanism

---
+++ src/lib/SongBook/components/ChordsChecks.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    export let lastChordsCheck: any;
+
+    const failures = lastChordsCheck?.fails.sort((a: any, b: any) =>
+        a.chord.url < b.chord.url ? 1 : -1
+    );
+</script>
+
+<div>
+    <h3>Urls checks</h3>
+    {#if !lastChordsCheck}
+        <span>Could not retrieve checks</span>
+    {:else}
+        <table>
+            <tr>
+                <th>Checks</th>
+                <th>Skipped</th>
+                <th>Errors</th>
+            </tr>
+            <tr>
+                <td>{lastChordsCheck.nbChecks}</td>
+                <td>{lastChordsCheck.nbSkipped}</td>
+                <td>{lastChordsCheck.nbFails}</td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <th>Status</th>
+                <th>Ref</th>
+                <th>Url</th>
+                <th>Data</th>
+            </tr>
+            {#each failures as failure}
+                <tr>
+                    <td>{failure.status}</td>
+                    <td>{failure.chord.artist} - {failure.chord.title}</td>
+                    <td><a href="{failure.chord.url}" target="_blank" rel="noopener noreferrer" >{failure.chord.url}</a></td>
+                    <td>{failure.error ? JSON.stringify(failure.error) : ''}</td>
+                </tr>
+            {/each}
+        </table>
+    {/if}
+</div>
--- src/routes/songbook/+page.server.ts
+++ src/routes/songbook/+page.server.ts
@@ -3,8 +3,8 @@ import type { Chord } from '$lib/SongBook/types';
 
 const CHORDS_URL = 'https://raw.githubusercontent.com/statox/blog/master/src/_data/chords.json';
 
-export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastCheckChords: any }> => {
-    const lastCheckChords = await getLastCheckChords();
+export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastChordsCheck: any }> => {
+    const lastChordsCheck = await getLastChordsCheck();
     const chords = await fetch(CHORDS_URL).then((response) => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -13,12 +13,12 @@ export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastCheckChor
         return response.json();
     });
     // TODO Add type checking
-    return { chords, lastCheckChords };
+    return { chords, lastChordsCheck };
 };
 
 const CHORDS_CHECK_URL = 'https://statox-backend-631a602a9744.herokuapp.com/checkChordsUrl';
-const getLastCheckChords = async () => {
+const getLastChordsCheck = async () => {
     return await fetch(CHORDS_CHECK_URL)
         .then((response) => response.json())
-        .catch((error) => error);
+        .catch((error) => null);
 };
