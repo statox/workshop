324adadd51b98cc9fc1ed3631f30ba7b5a967a4f
Adrien Fabre <adrien.fabre@dashlane.com>
2023-12-04 17:28:54 +0100

    Songbook: Dont fetch data on server

--- src/routes/songbook/+page.ts
+++ src/routes/songbook/+page.ts
@@ -1,10 +1,13 @@
+// TODO Fix import from $type and fix fetch function type
 import type { PageLoad } from './$types';
 import type { Chord } from '$lib/SongBook/types';
 
 const CHORDS_URL = 'https://raw.githubusercontent.com/statox/blog/master/src/_data/chords.json';
 
-export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastChordsCheck: any }> => {
-    const lastChordsCheck = await getLastChordsCheck();
+export const load: PageLoad = async ({
+    fetch
+}): Promise<{ chords: Chord[]; lastChordsCheck: any }> => {
+    const lastChordsCheck = await getLastChordsCheck(fetch);
     const chords = await fetch(CHORDS_URL).then((response) => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -17,7 +20,7 @@ export const load: PageLoad = async (): Promise<{ chords: Chord[]; lastChordsChe
 };
 
 const CHORDS_CHECK_URL = 'https://statox-backend-631a602a9744.herokuapp.com/checkChordsUrl';
-const getLastChordsCheck = async () => {
+const getLastChordsCheck = async (fetch: any) => {
     return await fetch(CHORDS_CHECK_URL)
         .then((response) => response.json())
         .catch((error) => null);
