41d137501a36f8e4bb301354a1cf48a716fd0f23
statox <me@statox.fr>
2023-12-10 12:02:57 +0000

    Deploying to gh-pages from @ statox/workshop@a11efef2d8ce3d11beb72a0f936aa4eafb6f8760 🚀

--- _app/immutable/chunks/paths.5ed47210.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_hz9ldl)==null?void 0:s.base)??"/workshop";var l;const e=((l=globalThis.__sveltekit_hz9ldl)==null?void 0:l.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/chunks/paths.fc24c2ed.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1goerdn)==null?void 0:s.base)??"/workshop";var e;const o=((e=globalThis.__sveltekit_1goerdn)==null?void 0:e.assets)??a;export{o as a,a as b};
