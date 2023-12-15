ffb9aa8a2877bbad978452c4e4af1912f95b1af6
Adrien Fabre <adrien.fabre@dashlane.com>
2023-12-15 02:04:43 +0100

    Metronome: Add info modal

---
+++ src/lib/Metronome/InfoModal.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import { closeModal } from '$lib/components/Modal';
+    export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+    <div role="dialog" class="modal">
+        <div class="contents">
+            <h4 class="title-bar">
+                Metronome
+                <button on:click={closeModal}>Close</button>
+            </h4>
+
+            <div>
+                This is a simple metronome:
+                <ul>
+                    <li>Use the play/pause button or the space bar to start and stop the metronome</li>
+                    <li>You can change the tempo by:</li>
+                    <ul>
+                        <li>Entering a number directly in the input</li>
+                        <li>Using the -5, -1, +1 and +5 buttons</li>
+                        <li>Taping a tempo on the tap button. When taping the tempo each tap counts for a subdivision if subdivisions are enabled. Stop taping for approx. 3 seconds to restart the tap counter.</li>
+                    </ul>
+                    <li>The "Beats in bar" setting change the number of beats counted by the metronome. The first beat is accented with a higher frequency tone.</li>
+                    <li>The "Subdivisions in beat" setting allow to change how the beats are divided. The subdivisions after the first one are played with a lower frequency tone.</li>
+                    <li>The visualization at the bottom follow the beats and their subdivisions.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .modal {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        margin: 3em;
+        z-index: 9999;
+
+        /* allow click-through to backdrop */
+        pointer-events: none;
+    }
+
+    .contents {
+        min-width: 240px;
+        border-radius: 26px;
+        padding: 16px;
+        background: white;
+        pointer-events: auto;
+
+        max-height: 90%;
+        overflow: auto;
+    }
+
+    .title-bar {
+        margin-bottom: 1em;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+</style>
