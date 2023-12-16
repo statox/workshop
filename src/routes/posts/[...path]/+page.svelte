a7e21c9af10b2db2ece590b8f0b2179c853823c8
Adrien Fabre <adrien.fabre@dashlane.com>
2023-12-16 05:23:36 +0100

    Routing: Add redirection to blog posts

---
+++ src/routes/posts/[...path]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+    import { PUBLIC_BLOG_URL } from '$env/static/public';
+
+    // We need to do the redirection on component mount on the client side
+    // Because doing that on the server side with the static adapter is not possible
+    // since all the possibles links are not referenced from this app
+
+    // The idea comes from this repo
+    // https://github.com/mohit2152sharma/svelte-mpa-example
+    onMount(() => {
+        const { path } = $page.params;
+        window.location.href = PUBLIC_BLOG_URL + /posts/ + path;
+    });
+</script>
