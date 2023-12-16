e4943e5b5e375d942ee9f9af20827dbc0651b72a
Adrien Fabre <adrien.fabre@dashlane.com>
2023-12-16 14:09:45 +0100

    Revert "Routing: Add redirection to blog posts"
    This reverts commit 1a6223bbba977ccaaaaa9f4fd793b435ae9c728c.

--- src/routes/posts/[...path]/+page.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import { PUBLIC_BLOG_URL } from '$env/static/public';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ params }) => {
-    const redirectUrl = PUBLIC_BLOG_URL + '/posts/' + params.path;
-
-    throw redirect(308, redirectUrl);
-};
