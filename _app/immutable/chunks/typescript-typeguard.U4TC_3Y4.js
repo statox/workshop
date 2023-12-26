036fd83fc464a74685e83108200591ca47b41e01
statox <me@statox.fr>
2023-12-26 18:55:53 +0000

    Deploying to gh-pages from @ statox/apps@2daec91b70c5c6a567fb2a28b76f68c5ddb1bda5 🚀

---
+++ _app/immutable/chunks/typescript-typeguard.U4TC_3Y4.js
@@ -0,0 +1,10 @@
+const t=`[tags]: # '["typescript", "type guard"]'
+[title]: # 'Typescript type guard function syntax'
+
+\`\`\`typescript
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+\`\`\`
+
+`;export{t as default};
