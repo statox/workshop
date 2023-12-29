7aec6a5481ce27b40e3ac08bff84685bdcb62d03
Adrien Fabre <adrien.fabre@dashlane.com>
2023-12-29 22:06:36 +0100

    NavBar: Rework items

---
+++ src/lib/components/NavItems/HomeNavItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+</script>
+
+<button title="Home" type="button" on:click={() => goto('/')}>
+    <i class="fa fa-home" aria-hidden="true"></i>
+</button>
+
+<style>
+    button {
+        background: none;
+        color: var(--nc-lk-1);
+        padding: 0;
+    }
+    button:hover {
+        background: none;
+        color: var(--nc-lk-2);
+    }
+    .fa {
+        font-size: 1.5em;
+    }
+</style>
