3efdbc9902f543aefcb3565f30ef1cc922903d87
Adrien Fabre <adrien.fabre@dashlane.com>
2024-01-02 17:27:46 +0100

    Clipboard: Rework public view

--- src/routes/clipboard/components/ClipboardPublicView.svelte
+++ src/routes/clipboard/components/ClipboardPublicView.svelte
@@ -1,24 +1,47 @@
 <script lang="ts">
+    import { toast } from '$lib/components/Toast';
     import { type ClipboardEntryEnriched } from '$lib/Clipboard/types';
+    import ExpirationInfo from './ExpirationInfo.svelte';
 
     export let clipboard: ClipboardEntryEnriched[];
+
+    const copyContent = (entry: ClipboardEntryEnriched) => {
+        navigator.clipboard.writeText(entry.content);
+        toast.push('<i class="fas fa-check"></i> Copied to clipboard', {
+            duration: 1000,
+            theme: {
+                '--toastBarHeight': 0
+            }
+        });
+    };
 </script>
 
 <div class="container">
-    <div>Creation</div>
-    <div>Name</div>
-    <div>Content</div>
-
     {#each clipboard.sort((a, b) => b.creationDateUnix - a.creationDateUnix) as entry}
-        <div>{entry.formatedCreationDate}</div>
+        <ExpirationInfo {entry} />
         <div>{entry.name}</div>
-        <div>{entry.content}</div>
+        <div class="entry-content" on:click={() => copyContent(entry)}>{entry.content}</div>
     {/each}
 </div>
 
 <style>
     .container {
         display: grid;
-        grid-template-columns: repeat(3, auto);
+        row-gap: 1em;
+    }
+
+    @media screen and (max-width: 750px) {
+        .container {
+            grid-template-columns: 100%;
+        }
+    }
+    @media screen and (min-width: 750px) {
+        .container {
+            grid-template-columns: 5% 25% 75%;
+        }
+    }
+
+    .entry-content {
+        cursor: pointer;
     }
 </style>
