e05d5dd2c5d5b241d38161b66b3ddf32823a0a3e
Adrien Fabre <adrien.fabre@dashlane.com>
2024-01-02 21:00:30 +0100

    Songbook: Display .doc documents in Office Web Viewer component

---
+++ src/routes/docviewer/[...path]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+
+    // Using the Office Web Viewer to display Microsoft documents
+    // currently used by the Song Book app to display .doc and .docx songs
+    // See https://www.microsoft.com/en-us/microsoft-365/blog/2013/04/10/office-web-viewer-view-office-documents-in-a-browser/
+
+    let innerWidth: number;
+
+    const { path } = $page.params;
+    const src = `https://view.officeapps.live.com/op/embed.aspx?src=${path}`;
+
+    $: viewWidth = Math.min(innerWidth, 700);
+</script>
+
+<div id="viewer">
+    <iframe title={path} {src} width={viewWidth} height="900px" frameborder="0"> document </iframe>
+</div>
+
+<svelte:window bind:innerWidth />
