1475578a5d5854b5d79196820762fbee12da9edc
Adrien Fabre <adrien.fabre@dashlane.com>
2024-01-03 01:17:05 +0100

    Clipboard: Improve TTL input

--- src/routes/clipboard/components/ClipboardForm.svelte
+++ src/routes/clipboard/components/ClipboardForm.svelte
@@ -2,13 +2,19 @@
     import { createEventDispatcher } from 'svelte';
     import { user } from '$lib/auth/service';
     import { uploadToClipboard } from '$lib/Clipboard/api';
+    import { Duration, type DurationLikeObject } from 'luxon';
 
     const dispatch = createEventDispatcher();
 
     let name: string;
     let content: string;
-    let ttlSeconds = 3600;
     let isPublic = false;
+
+    const ttlUnits = ['minutes', 'hours', 'days'];
+    const ttlInput = {
+        value: 10,
+        unit: ttlUnits[0]
+    }
 
     let error: string;
     const upload = () => {
@@ -20,6 +26,11 @@
             error = 'content should be defined';
             return;
         }
+
+        const durationLikeObj: DurationLikeObject = {};
+        durationLikeObj[ttlInput.unit as 'minutes'|'hours'|'days'] = ttlInput.value;
+        const ttlDuration = Duration.fromObject(durationLikeObj);
+        let ttlSeconds = ttlDuration.as('seconds');
         if (ttlSeconds < 0) {
             error = 'TTL should be positive';
             return;
@@ -45,8 +56,13 @@
     </p>
 
     <p>
-        <label for="ttlSeconds">TTL (seconds)</label>
-        <input type="number" min="0" bind:value={ttlSeconds} />
+        <label for="ttlSeconds">TTL</label>
+        <input type="number" min="0" bind:value={ttlInput.value} />
+        <select bind:value={ttlInput.unit}>
+            {#each ttlUnits as unit}
+                <option value={unit}>{unit}</option>
+            {/each}
+        </select>
     </p>
 
     <p>
