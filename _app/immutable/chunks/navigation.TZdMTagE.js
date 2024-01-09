64e6759458a1e11853694c1a4b36bb0b0763b7aa
statox <me@statox.fr>
2024-01-09 21:00:23 +0000

    Deploying to gh-pages from @ statox/apps@cbc914ed0e766dbe4c75f34b083b6bfa48060d83 🚀

---
+++ _app/immutable/chunks/navigation.TZdMTagE.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.MZj6Uqhm.js";const e=o("goto");export{e as g};
--- _app/immutable/chunks/paths.aVX1Fni-.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_7xjvgf?.base??"",a=globalThis.__sveltekit_7xjvgf?.assets??s;export{a,s as b};
