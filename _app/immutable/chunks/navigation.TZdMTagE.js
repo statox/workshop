5786bcf38e7c8e726d5290f28a226641c2992bd0
statox <me@statox.fr>
2024-01-10 21:23:04 +0000

    Deploying to gh-pages from @ statox/apps@7ec6bbfc2ef3b14187259ff0faca2025c15c3c22 🚀

--- _app/immutable/chunks/navigation.TZdMTagE.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.MZj6Uqhm.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/paths.P_FIzUMO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_19h85iv?.base??"",a=globalThis.__sveltekit_19h85iv?.assets??s;export{a,s as b};
