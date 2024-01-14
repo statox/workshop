cf749b567a8b4a71ecbfbfbd7dda50da677e01ef
Adrien Fabre <adrien.fabre@dashlane.com>
2024-01-14 02:24:20 +0100

    Clipboard: Tweak input form

--- src/routes/clipboard/components/ClipboardForm.svelte
+++ src/routes/clipboard/components/ClipboardForm.svelte
@@ -58,35 +58,38 @@
 </script>
 
 {#if $user}
-    <p>
-        <label for="name">Name</label>
-        <input type="text" bind:value={name} />
-    </p>
+    <div class="meta-section">
+        <p class="meta-section-item">
+            <label for="name">Name</label>
+            <input type="text" bind:value={name} />
+        </p>
+        <p class="meta-section-item">
+            <label for="content">Content</label>
+            <input type="textarea" bind:value={content} />
+        </p>
+    </div>
 
-    <p>
-        <label for="content">Content</label>
-        <input type="text" bind:value={content} />
-    </p>
-
-    <p>
+    <p class="file-section">
         <label for="file">File</label>
-        <input type="file" bind:files />
+        <input class="file-input" type="file" bind:files />
     </p>
 
-    <p>
-        <label for="ttlSeconds">TTL</label>
-        <input type="number" min="0" bind:value={ttlInput.value} />
-        <select bind:value={ttlInput.unit}>
-            {#each ttlUnits as unit}
-                <option value={unit}>{unit}</option>
-            {/each}
-        </select>
-    </p>
+    <div class="visibility-section">
+        <p class="visibility-section-item">
+            <label for="ttlSeconds">TTL</label>
+            <input type="number" min="0" bind:value={ttlInput.value} />
+            <select bind:value={ttlInput.unit}>
+                {#each ttlUnits as unit}
+                    <option value={unit}>{unit}</option>
+                {/each}
+            </select>
+        </p>
 
-    <p>
-        <label for="isPublic">Public</label>
-        <input type="checkbox" bind:checked={isPublic} />
-    </p>
+        <p class="visibility-section-item">
+            <label for="isPublic">Public</label>
+            <input type="checkbox" bind:checked={isPublic} />
+        </p>
+    </div>
 
     <p>
         <button on:click={upload}>Upload</button>
@@ -94,3 +97,45 @@
 {:else}
     <p>Login to upload content</p>
 {/if}
+
+<style>
+    .meta-section {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .visibility-section {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .file-section {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .file-input {
+        flex-grow: 2;
+    }
+
+    @media screen and (max-width: 750px) {
+        .meta-section-item {
+            flex: 1 0 100%;
+        }
+        .meta-section-item > input {
+            width: 100%;
+        }
+        .visibility-section-item {
+            flex: 1 0 100%;
+        }
+    }
+    @media screen and (min-width: 750px) {
+        .meta-section-item {
+            flex: 1 0 50%; /* This will make the items take up 50% of the container's width, effectively creating two columns */
+        }
+        .meta-section-item > input {
+            width: 100%;
+        }
+        .visibility-section-item {
+            flex: 1 0 50%; /* This will make the items take up 50% of the container's width, effectively creating two columns */
+        }
+    }
+
+</style>
