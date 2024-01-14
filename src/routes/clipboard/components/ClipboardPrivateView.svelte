33c0b6bbece4c1ea38a12fbb86fc462495261714
Adrien Fabre <adrien.fabre@dashlane.com>
2024-01-14 01:04:59 +0100

    Clipboard: Extract info to component

--- src/routes/clipboard/components/ClipboardPrivateView.svelte
+++ src/routes/clipboard/components/ClipboardPrivateView.svelte
@@ -1,34 +1,17 @@
 <script lang="ts">
     import { type ClipboardEntryEnriched } from '$lib/Clipboard/types';
-    import { deleteClipboardEntry } from '$lib/Clipboard/api';
     import { createEventDispatcher } from 'svelte';
-    import ExpirationInfo from './ExpirationInfo.svelte';
     import EntryContentComponent from './EntryContentComponent.svelte';
     import EntryFileComponent from './EntryFileComponent.svelte';
+    import EntryInfoComponent from './EntryInfoComponent.svelte';
 
     const dispatch = createEventDispatcher();
     export let clipboard: ClipboardEntryEnriched[];
-
-    const deleteEntry = (name: string) => {
-        deleteClipboardEntry(name).then((_response) => dispatch('delete'));
-    };
 </script>
 
 <div class="container">
     {#each clipboard.sort((a, b) => b.creationDateUnix - a.creationDateUnix) as entry}
-        <div class="info-container">
-            <ExpirationInfo {entry} />
-            <input
-                class="is-public-checkbox"
-                type="checkbox"
-                bind:checked={entry.isPublic}
-                disabled
-            />
-            <button class="delete-button" on:click={() => deleteEntry(entry.name)}>
-                <i class="fas fa-trash-alt"></i>
-            </button>
-            <div>{entry.formatedCreationDate}</div>
-        </div>
+        <EntryInfoComponent {entry} on:delete={() => dispatch('delete')} />
         <div>{entry.name}</div>
         <EntryContentComponent {entry} />
         <EntryFileComponent {entry} />
@@ -50,22 +33,5 @@
         .container {
             grid-template-columns: 25% 25% 35% 35%;
         }
-    }
-
-    .info-container {
-        display: flex;
-        flex-direction: row;
-        gap: 10px 10px;
-        justify-content: flex-start;
-    }
-    .delete-button {
-        background: red;
-        color: white;
-        padding: 0;
-        width: 2em;
-        height: 2em;
-    }
-    .is-public-checkbox {
-        height: 2em;
     }
 </style>
