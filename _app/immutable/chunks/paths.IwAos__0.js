3400faa3d60aabfb61c2eafae7484ba2d77b706d
statox <me@statox.fr>
2024-02-27 23:27:47 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@420bc8e871d67939d7b0a215a6d37f2e23a69759 🚀

---
+++ _app/immutable/chunks/paths.IwAos__0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1e41i8o?.base??"",e=globalThis.__sveltekit_1e41i8o?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/paths.zB_ps1uf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_18vygcd?.base??"",a=globalThis.__sveltekit_18vygcd?.assets??s;export{a,s as b};
