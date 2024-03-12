36b42e6bb6d9019aa055c845d1a9128548854b73
Adrien Fabre <adrien.fabre@dashlane.com>
2024-03-12 21:22:55 +0100

    ChordLink: Remove debug logging

--- src/routes/songbook/components/ChordLink.svelte
+++ src/routes/songbook/components/ChordLink.svelte
@@ -35,28 +35,17 @@
 
     const addVisit = async () => {
         try {
-            debugger; // AFA
-            console.log('about to add visit');
             await uploadLinkVisit(chord.url);
-            console.log('added visit');
 
             // Once we made the call and it succeeded update the store
             // to reflect the change without having to call the API again
             const map = get(visitCountsStore);
-            console.log('pouet 1');
             const data = map.get(chord.url) || { count: 0, lastAccessDateUnix: 0 };
-            console.log('pouet 2');
             data.count++;
-            console.log('pouet 3');
             data.lastAccessDateUnix = Date.now() / 1000;
-            console.log('pouet 4');
             map.set(chord.url, data);
-            console.log('pouet 5');
             visitCountsStore.set(map);
-            console.log('pouet 6');
         } catch (error) {
-            console.log('got an error');
-            console.log(error);
             const message = `<strong>Visit not counted</strong><br/> ${(error as Error).message}`;
             toast.push(message, {
                 // Don't automatically dismiss to see the error when coming back
