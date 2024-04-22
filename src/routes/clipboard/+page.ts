4daae911b753dc19d9c1267ad6e274c43a1a9b71
Adrien Fabre <adrien.fabre@dashlane.com>
2024-04-22 17:18:07 +0200

    Routes: Disable SSR for pages calling API
    This is to avoid calling the API at build time to prepare for better CI
    steps

---
+++ src/routes/clipboard/+page.ts
@@ -0,0 +1,2 @@
+// Avoid calling API's /clipboard/getPublicEntries at build time
+export const ssr = false;
--- src/routes/songbook/edit/+page.ts
+++ src/routes/songbook/edit/+page.ts
@@ -2,6 +2,7 @@ import { getChords } from '$lib/Songbook/api';
 import type { RawChord } from '$lib/Songbook/types';
 import type { PageLoad } from './$types';
 
+export const ssr = false; // Avoid calling API's /chords/getAll at build time
 export const load: PageLoad = async (): Promise<{ chords: RawChord[] }> => {
     const chords = await getChords();
     return { chords };
