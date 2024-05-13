22f35fe2e0452bc36f5f69164dcc81deacea550c
statox <me@statox.fr>
2024-05-13 17:57:42 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@3f24f4ae06a76fbf0136a4b53c2a815d50d4b118 🚀

---
+++ _app/immutable/chunks/npm_depcheck._6INoLcK.js
@@ -0,0 +1,9 @@
+const e=`[tags]: # '["npm", "dependencies", "javascript", "package.json"]'
+[title]: # 'NPM: Check unused dependencies'
+
+The npm package [depcheck](https://www.npmjs.com/package/depcheck) is great for that:
+
+\`\`\`bash
+npx depcheck
+\`\`\`
+`;export{e as default};
