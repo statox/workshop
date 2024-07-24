f6b9dddaa12ab5b217ae0499b67565829264bdb9
Adrien Fabre <adrien.fabre@dashlane.com>
2024-07-24 23:08:22 +0200

    ButtonDelete - Automatically reset button if not confirmed after 5 seconds

--- src/lib/components/ButtonDelete/Main.svelte
+++ src/lib/components/ButtonDelete/Main.svelte
@@ -3,16 +3,26 @@
 
     type State = 'initial' | 'triggered' | 'confirmed';
     let state: State = 'initial';
+    let resetStateTimeout: ReturnType<typeof setTimeout>;
+    const RESET_TIMEOUT_SECONDS = 5;
 
     const dispatch = createEventDispatcher();
+    const trigger = () => {
+        state = 'triggered';
+        resetStateTimeout = setTimeout(reset, RESET_TIMEOUT_SECONDS * 1000);
+    };
     const confirmDelete = () => {
+        clearTimeout(resetStateTimeout);
         state = 'confirmed';
         dispatch('delete');
+    };
+    const reset = () => {
+        state = 'initial';
     };
 </script>
 
 {#if state === 'initial'}
-    <button class="delete-button" on:click={() => (state = 'triggered')} title="Delete?">
+    <button class="delete-button" on:click={trigger} title="Delete?">
         <i class="fas fa-trash-alt"></i>
     </button>
 {:else if state === 'triggered'}
