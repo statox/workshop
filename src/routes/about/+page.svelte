e26d93527271e2615b78e99b5c1f0ea93d483c88
Adrien Fabre <adrien.fabre@dashlane.com>
2024-07-31 00:27:26 +0200

    About - Add a description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,43 @@
     export let data: BuildInfo;
 </script>
 
-<div class="container">
+<h2>About this website</h2>
+
+<div class="description">
+    <div>
+        This website is a collection of application I created for myself. You can access some
+        features (mostly reading non-sensitive data) but any modification or access to sensitive
+        data (theoretically) requires to login as myself, so you shouldn't be able to do it. (For
+        now the authentication is handled via <a target="_blank" href="https://auth0.com/">auth0</a>
+        and uses Github as the identity provider.
+    </div>
+    <br />
+    <div>
+        If you are trying to find out more about me, you probably want to check out my blog
+        <a target="_blank" href="https://www.statox.fr/">statox.fr</a> where I discuss some of the applications
+        I created here as well as other projects.
+    </div>
+    <br />
+    <div>
+        If you are curious about this website the sources are
+        <a target="_blank" href="https://github.com/statox/apps.statox.fr">on Github</a>. It is
+        deployed via Github pages (see the badge below for the latest build status and a quick
+        access to the CI) and made with
+        <a target="_blank" href="https://svelte.dev/">SvelteJS</a>.
+    </div>
+    <br />
+    <div>
+        You can also find the sources of
+        <a target="_blank" href="https://github.com/statox/api.statox.fr">the api I created</a> to
+        power this website (and other projects). The API is hosted on Heroku, is made with
+        <a target="_blank" href="https://expressjs.com/">ExpressJS</a> and uses a MySQL db, cloudflare
+        R2 storage and various other technologies.
+    </div>
+</div>
+
+<br />
+
+<div class="build-info">
     <a target="_blank" rel="noopener noreferrer" href="https://github.com/statox/apps/actions">
         <img
             class="build-status-badge"
@@ -25,7 +61,7 @@
 </div>
 
 <style>
-    .container {
+    .build-info {
         display: flex;
         justify-content: space-around;
     }
