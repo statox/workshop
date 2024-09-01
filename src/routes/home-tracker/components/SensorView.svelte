affd61dbedcf82507361571f309b0342cfafc584
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-02 01:28:10 +0200

    HomeTracker - Add svelte-chartjs dependency and start graphing data

--- src/routes/home-tracker/components/SensorView.svelte
+++ src/routes/home-tracker/components/SensorView.svelte
@@ -1,14 +1,82 @@
 <script lang="ts">
+    import { Line } from 'svelte-chartjs';
     import type { SensorRecord } from '$lib/HomeTracker/types';
     import { DateTime } from 'luxon';
+    import {
+        Chart as ChartJS,
+        Title,
+        Tooltip,
+        Legend,
+        LineElement,
+        LinearScale,
+        PointElement,
+        CategoryScale
+    } from 'chart.js';
+
+    ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale);
 
     export let sensorName: string;
     export let records: SensorRecord[];
 
-    const lastRecord = records.sort((a, b) => b['@timestamp'] - a['@timestamp'])[0];
+    const lastRecord = records.sort((a, b) => a['@timestamp'] - b['@timestamp'])[
+        records.length - 1
+    ];
 
     const formatTimestamp = (ts: number) => {
         return DateTime.fromMillis(ts).toRelative();
+    };
+
+    const dataTemp = {
+        labels: records.map((r) => DateTime.fromMillis(r['@timestamp']).toFormat('HH:mm')),
+        datasets: [
+            {
+                label: 'Temp (C)',
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: 'rgba(225, 204, 230, .3)',
+                borderColor: 'rgb(205, 130, 158)',
+                borderCapStyle: 'butt' as const,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter' as const,
+                pointBorderColor: 'rgb(205, 130, 158)',
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+                pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: records.map((r) => r.document.tempCelsius!)
+            }
+        ]
+    };
+    const dataHumidity = {
+        labels: records.map((r) => DateTime.fromMillis(r['@timestamp']).toFormat('HH:mm')),
+        datasets: [
+            {
+                label: 'Humidity',
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: 'rgba(42, 163, 221, .3)',
+                borderColor: 'rgb(42, 163, 221)',
+                borderCapStyle: 'butt' as const,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter' as const,
+                pointBorderColor: 'rgb(35, 26, 136)',
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+                pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: records.map((r) => r.document.humidity!)
+            }
+        ]
     };
 </script>
 
@@ -29,7 +97,6 @@
         <p>Internal Humidity: {lastRecord.document.internalHumidity.toFixed(2)} %</p>
     {/if}
 
-    <!-- {#each records as record} -->
-    <!--     <p>{JSON.stringify(record)}</p> -->
-    <!-- {/each} -->
+    <Line data={dataTemp} height={50} options={{ responsive: true }} />
+    <Line data={dataHumidity} height={50} options={{ responsive: true }} />
 </div>
