55f5fb7f9c96a290e5162bb218847e701ddf5f1a
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-02 13:12:04 +0200

    HomeTracker - Try to debug page blocking on "Loading data" message

--- src/lib/HomeTracker/api.ts
+++ src/lib/HomeTracker/api.ts
@@ -2,7 +2,12 @@ import { requestAPIGet } from '$lib/api';
 import type { HomeTrackerLatestResponse } from './types';
 
 export const getHomeTrackerLatest = async () => {
-    return await requestAPIGet<HomeTrackerLatestResponse>({
+    console.log('getHomeTrackerLatest()');
+    const result = await requestAPIGet<HomeTrackerLatestResponse>({
         path: '/homeTracker/getLatest'
     });
+
+    console.log('Got latest data:');
+    console.log({ result });
+    return result;
 };
--- src/routes/home-tracker/+page.svelte
+++ src/routes/home-tracker/+page.svelte
@@ -2,15 +2,13 @@
     import { HeadIOS } from '$lib/components/HeadIOS';
     import { getHomeTrackerLatest } from '$lib/HomeTracker/api';
     import SensorView from './components/SensorView.svelte';
-
-    let latestDataAPI = getHomeTrackerLatest();
 </script>
 
 <HeadIOS title="Home Tracker" description="Recording of my sensors" />
 
 <h2>Home Tracker</h2>
 
-{#await latestDataAPI}
+{#await getHomeTrackerLatest()}
     <p>Loading data</p>
 {:then latestData}
     {#each Object.keys(latestData.recordsBySensor) as sensor}
