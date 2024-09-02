3d26c9176483ff3cdfa37232b32d900276068eb8
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-02 23:58:19 +0200

    HomeTracker - Add time window selection buttons

--- src/lib/HomeTracker/api.ts
+++ src/lib/HomeTracker/api.ts
@@ -1,10 +1,10 @@
-import { requestAPIGet } from '$lib/api';
+import { requestAPIPost } from '$lib/api';
 import type { HomeTrackerLatestResponse } from './types';
 
-export const getHomeTrackerLatest = async () => {
-    console.log('getHomeTrackerLatest()');
-    const result = await requestAPIGet<HomeTrackerLatestResponse>({
-        path: '/homeTracker/getLatest'
+export const getHomeTrackerLatest = async (timeWindow: '3h' | '12h' | '1d' | '3d' | '7d') => {
+    const result = await requestAPIPost<HomeTrackerLatestResponse>({
+        path: '/homeTracker/getLatest',
+        data: { timeWindow }
     });
 
     console.log('Got latest data:');
--- src/routes/home-tracker/+page.svelte
+++ src/routes/home-tracker/+page.svelte
@@ -3,15 +3,27 @@
     import { getHomeTrackerLatest } from '$lib/HomeTracker/api';
     import SensorsSummary from './components/SensorsSummary.svelte';
     import MultiSensorsGraph from './components/MultiSensorsGraph.svelte';
+
+    let latestDataApi = getHomeTrackerLatest('1d');
+    const refreshData = (timeWindow: '3h' | '12h' | '1d' | '3d' | '7d') =>
+        (latestDataApi = getHomeTrackerLatest(timeWindow));
 </script>
 
 <HeadIOS title="Home Tracker" description="Recording of my sensors" />
 
 <h2>Home Tracker</h2>
 
-{#await getHomeTrackerLatest()}
+{#await latestDataApi}
     <p>Loading data</p>
 {:then latestData}
+    <div>
+        <button on:click={() => refreshData('3h')}>3 hours</button>
+        <button on:click={() => refreshData('12h')}>12 hours</button>
+        <button on:click={() => refreshData('1d')}>1 day</button>
+        <button on:click={() => refreshData('3d')}>3 days</button>
+        <button on:click={() => refreshData('7d')}>7 days</button>
+    </div>
+    <br />
     <SensorsSummary recordsBySensor={latestData.recordsBySensor} />
     <br />
     <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="temperature" />
