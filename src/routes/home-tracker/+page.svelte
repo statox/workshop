ce1c78021e064da3c184270b687563d77872ff07
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-02 21:55:07 +0200

    HomeTracker - Rework visualization

--- src/routes/home-tracker/+page.svelte
+++ src/routes/home-tracker/+page.svelte
@@ -2,7 +2,7 @@
     import { HeadIOS } from '$lib/components/HeadIOS';
     import { getHomeTrackerLatest } from '$lib/HomeTracker/api';
     import SensorsSummary from './components/SensorsSummary.svelte';
-    import SensorView from './components/SensorView.svelte';
+    import MultiSensorsGraph from './components/MultiSensorsGraph.svelte';
 </script>
 
 <HeadIOS title="Home Tracker" description="Recording of my sensors" />
@@ -12,10 +12,11 @@
 {#await getHomeTrackerLatest()}
     <p>Loading data</p>
 {:then latestData}
-    {#each Object.keys(latestData.recordsBySensor) as sensor}
-        <SensorsSummary recordsBySensor={latestData.recordsBySensor} />
-        <SensorView sensorName={sensor} records={latestData.recordsBySensor[sensor]} />
-    {/each}
+    <SensorsSummary recordsBySensor={latestData.recordsBySensor} />
+    <br />
+    <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="temperature" />
+    <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="humidity" />
+    <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="pressure" />
 {:catch error}
     <p>Something went wrong</p>
     <p>{JSON.stringify(error)}</p>
---
+++ src/routes/home-tracker/components/MultiSensorsGraph.svelte
@@ -0,0 +1,165 @@
+<script lang="ts">
+    import { DateTime } from 'luxon';
+    import { Line } from 'svelte-chartjs';
+    import type { SensorRecord } from '$lib/HomeTracker/types';
+    import {
+        Chart as ChartJS,
+        Title,
+        Tooltip,
+        Legend,
+        LineElement,
+        LinearScale,
+        PointElement,
+        CategoryScale
+    } from 'chart.js';
+    import type { RecordsBySensor, SensorRecord } from '$lib/HomeTracker/types';
+
+    ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale);
+
+    const formatTimestampToHuman = (ts: number) => {
+        const time = DateTime.fromMillis(ts);
+        if (time.diffNow('hours').hours < 12) {
+            return DateTime.fromMillis(ts).toFormat('HH:mm');
+        } else {
+            return DateTime.fromMillis(ts).toFormat('dd/MM HH:mm');
+        }
+    };
+
+    export let recordsBySensor: RecordsBySensor;
+    export let metric: 'temperature' | 'humidity' | 'pressure';
+
+    const allDates = Object.keys(recordsBySensor)
+        .reduce((allTimes, sensor) => {
+            const sensorRecords = recordsBySensor[sensor];
+            const sensorTimes = sensorRecords.map((r: SensorRecord) => r['@timestamp']);
+            return allTimes.concat(sensorTimes);
+        }, [])
+        .sort((a, b) => a - b)
+        .map((ts) => formatTimestampToHuman(ts));
+
+    const indexColors = [
+        [205, 130, 158],
+        [130, 205, 158],
+        [130, 158, 205]
+    ];
+
+    const getColorString = (color: number[], internalData: boolean) => {
+        return `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${internalData ? '0.3' : '1'})`;
+    };
+
+    const datasets = Object.keys(recordsBySensor).reduce((datasets, sensor, index) => {
+        const sensorRecords = recordsBySensor[sensor];
+        const data = sensorRecords.map((record: SensorRecord) => {
+            let recordMetric: number | undefined;
+            if (metric === 'temperature') {
+                recordMetric = record.document.tempCelsius;
+            } else if (metric === 'humidity') {
+                recordMetric = record.document.humidity;
+            } else {
+                recordMetric = record.document.pressurehPa;
+            }
+            return {
+                x: formatTimestampToHuman(record['@timestamp']),
+                y: recordMetric
+            };
+        });
+
+        // @ts-expect-error TODO Fix that
+        datasets.push({
+            label: sensor,
+            fill: true,
+            lineTension: 0.3,
+            backgroundColor: 'rgba(225, 204, 230, .3)',
+            borderColor: getColorString(indexColors[index], false),
+            borderCapStyle: 'butt' as const,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter' as const,
+            pointBorderColor: getColorString(indexColors[index], false),
+            pointBackgroundColor: 'rgb(255, 255, 255)',
+            pointBorderWidth: 10,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+            pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data
+        });
+
+        if (metric === 'temperature' && sensorRecords[0].document.internalTempCelsius) {
+            const internalData = sensorRecords.map((record: SensorRecord) => {
+                return {
+                    x: formatTimestampToHuman(record['@timestamp']),
+                    y: record.document.internalTempCelsius
+                };
+            });
+
+            // @ts-expect-error TODO Fix that
+            datasets.push({
+                label: sensor + ' (int)',
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: 'rgba(225, 204, 230, .3)',
+                borderColor: getColorString(indexColors[index], true),
+                borderCapStyle: 'butt' as const,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter' as const,
+                pointBorderColor: getColorString(indexColors[index], true),
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+                pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: internalData
+            });
+        }
+        if (metric === 'humidity' && sensorRecords[0].document.internalHumidity) {
+            const internalData = sensorRecords.map((record: SensorRecord) => {
+                return {
+                    x: formatTimestampToHuman(record['@timestamp']),
+                    y: record.document.internalHumidity
+                };
+            });
+
+            // @ts-expect-error TODO Fix that
+            datasets.push({
+                label: sensor + ' (int)',
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: 'rgba(225, 204, 230, .3)',
+                borderColor: getColorString(indexColors[index], true),
+                borderCapStyle: 'butt' as const,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter' as const,
+                pointBorderColor: getColorString(indexColors[index], true),
+                pointBackgroundColor: 'rgb(255, 255, 255)',
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+                pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: internalData
+            });
+        }
+
+        return datasets;
+    }, []);
+
+    const dataTemp = {
+        labels: allDates,
+        datasets
+    };
+</script>
+
+<h3>{metric}</h3>
+<div>
+    <Line data={dataTemp} height={50} options={{ responsive: true }} />
+</div>
