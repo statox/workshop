fa815b8d8503370d407c32be1ebb7d3b36042fab
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-03 11:23:05 +0200

    HomeTracker - Tweak time window selection to be responsive

--- src/routes/home-tracker/+page.svelte
+++ src/routes/home-tracker/+page.svelte
@@ -16,15 +16,15 @@
 {#await latestDataApi}
     <p>Loading data</p>
 {:then latestData}
-    <div>
+    <SensorsSummary recordsBySensor={latestData.recordsBySensor} />
+    <br />
+    <div class="time-window-select">
         <button on:click={() => refreshData('3h')}>3 hours</button>
         <button on:click={() => refreshData('12h')}>12 hours</button>
         <button on:click={() => refreshData('1d')}>1 day</button>
         <button on:click={() => refreshData('3d')}>3 days</button>
         <button on:click={() => refreshData('7d')}>7 days</button>
     </div>
-    <br />
-    <SensorsSummary recordsBySensor={latestData.recordsBySensor} />
     <br />
     <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="temperature" />
     <MultiSensorsGraph recordsBySensor={latestData.recordsBySensor} metric="humidity" />
@@ -34,3 +34,17 @@
     <p>Something went wrong</p>
     <p>{JSON.stringify(error)}</p>
 {/await}
+
+<style>
+    .time-window-select {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        gap: 20px;
+    }
+    @media (max-width: 768px) {
+        .time-window-select {
+            justify-content: space-between;
+        }
+    }
+</style>
