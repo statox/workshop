22deffc5db4c890b15a160e163a89108af55b7c6
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-18 18:19:12 +0200

    HomeTracker - Add guard against not logged in user

--- src/routes/home-tracker/+page.svelte
+++ src/routes/home-tracker/+page.svelte
@@ -6,6 +6,8 @@
     import { DateTime } from 'luxon';
     import type { HomeTrackerTimeData } from '$lib/HomeTracker/types';
     import { pageNameStore } from '$lib/components/Header';
+    import { user } from '$lib/auth/service';
+    import { Notice } from '$lib/components/Notice';
 
     pageNameStore.set('Home Tracker');
 
@@ -33,51 +35,68 @@
 
 <HeadIOS title="Home Tracker" description="Recording of my sensors" />
 
-<div>
-    <button on:click={() => (apiData = refreshData(timeWindow))}>Refresh</button>
-    <span style={'font-weight: bolder'}>Last Refresh</span>
-    <span>{lastRefreshDate?.toFormat('dd/MM HH:mm') || 'NA'}</span>
-</div>
-<br />
-
-{#await apiData}
-    <p>Loading sensors data</p>
-{:then { histogramData, sensorsDetails }}
-    <SensorsSummary sensorsData={sensorsDetails.sensors} />
-    <br />
-    <div class="time-window-select">
-        <button class:selected={timeWindow === '3h'} on:click={() => (apiData = refreshData('3h'))}>
-            3 hours
-        </button>
-        <button
-            class:selected={timeWindow === '12h'}
-            on:click={() => (apiData = refreshData('12h'))}
-        >
-            12 hours
-        </button>
-        <button class:selected={timeWindow === '1d'} on:click={() => (apiData = refreshData('1d'))}>
-            1 day
-        </button>
-        <button class:selected={timeWindow === '3d'} on:click={() => (apiData = refreshData('3d'))}>
-            3 days
-        </button>
-        <button class:selected={timeWindow === '7d'} on:click={() => (apiData = refreshData('7d'))}>
-            7 days
-        </button>
+{#if $user}
+    <div>
+        <button on:click={() => (apiData = refreshData(timeWindow))}>Refresh</button>
+        <span style={'font-weight: bolder'}>Last Refresh</span>
+        <span>{lastRefreshDate?.toFormat('dd/MM HH:mm') || 'NA'}</span>
     </div>
+
     <br />
-    {#each metrics as metric}
-        <MultiSensorsGraph
-            sensorsData={sensorsDetails.sensors}
-            histogramData={histogramData.histogramData}
-            sensorNames={histogramData.sensorNames}
-            {metric}
-        />
-    {/each}
-{:catch error}
-    <p>Something went wrong getting sensor data</p>
-    <p>{JSON.stringify(error)}</p>
-{/await}
+
+    {#await apiData}
+        <p>Loading sensors data</p>
+    {:then { histogramData, sensorsDetails }}
+        <SensorsSummary sensorsData={sensorsDetails.sensors} />
+        <br />
+        <div class="time-window-select">
+            <button
+                class:selected={timeWindow === '3h'}
+                on:click={() => (apiData = refreshData('3h'))}
+            >
+                3 hours
+            </button>
+            <button
+                class:selected={timeWindow === '12h'}
+                on:click={() => (apiData = refreshData('12h'))}
+            >
+                12 hours
+            </button>
+            <button
+                class:selected={timeWindow === '1d'}
+                on:click={() => (apiData = refreshData('1d'))}
+            >
+                1 day
+            </button>
+            <button
+                class:selected={timeWindow === '3d'}
+                on:click={() => (apiData = refreshData('3d'))}
+            >
+                3 days
+            </button>
+            <button
+                class:selected={timeWindow === '7d'}
+                on:click={() => (apiData = refreshData('7d'))}
+            >
+                7 days
+            </button>
+        </div>
+        <br />
+        {#each metrics as metric}
+            <MultiSensorsGraph
+                sensorsData={sensorsDetails.sensors}
+                histogramData={histogramData.histogramData}
+                sensorNames={histogramData.sensorNames}
+                {metric}
+            />
+        {/each}
+    {:catch error}
+        <p>Something went wrong getting sensor data</p>
+        <p>{error}</p>
+    {/await}
+{:else}
+    <Notice item={{ level: 'info', header: 'Login to access data' }} />
+{/if}
 
 <style>
     button.selected {
